0669e4ed49ce9d135503a41fb665a14f80cef8d9
babie <babie.wildheart@gmail.com>
2020-10-10 04:38:52 +0900

    feat: Add modern-css-reset package

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import 'modern-css-reset'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
