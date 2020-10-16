3ce0f0075603723aa544caed77ad983a8c00d7d6
babie <babie.wildheart@gmail.com>
2020-10-17 04:32:22 +0900

    refactor: Rename `remark-parse`'s import name from `markdown` to `md2remark`

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,12 +1,12 @@
 import React from 'react'
 import unified from 'unified'
-import markdown from 'remark-parse'
+import md2remark from 'remark-parse'
 import remark2rehype from 'remark-rehype'
 import rehype2react from 'rehype-react'
 
 export const md2react = (md: string): React.ReactElement => {
   const processor = unified()
-    .use(markdown)
+    .use(md2remark)
     .use(remark2rehype)
     .use(rehype2react, {
       createElement: React.createElement,
