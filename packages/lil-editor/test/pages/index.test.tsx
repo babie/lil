c47eab2c3cd04379bb03cdc666d673da6f6d1ba7
babie <babie.wildheart@gmail.com>
2020-10-19 12:42:16 +0900

    test(lil-editor): Fix snapshot test error

--- packages/lil-editor/test/pages/index.test.tsx
+++ packages/lil-editor/test/pages/index.test.tsx
@@ -1,18 +1,14 @@
-//import React from 'react'
-//import { render, fireEvent } from '../testUtils'
+import React from 'react'
+import { render } from '../testUtils'
 import { Home } from '../../pages/index'
 
 describe('Home page', () => {
-  it('always ok', () => {
-    ;<Home />
-    expect('ok').toMatch('ok')
-  })
-  /*
   it('matches snapshot', () => {
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
+  /*
   it('clicking button triggers alert', () => {
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
