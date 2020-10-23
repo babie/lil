d8701a312ef9cbc38d27d2f1c15805eed6e6c36f
babie <babie.wildheart@gmail.com>
2020-10-23 16:30:51 +0900

    chore(lil-viewer): Remove `yarn` from lint-staged commands and Call directly eslint and prettier

--- packages/lil-viewer/lint-staged.config.js
+++ packages/lil-viewer/lint-staged.config.js
@@ -2,8 +2,8 @@ module.exports = {
   '*.@(js|jsx|ts|tsx)': (filenames) => {
     const wrapped = filenames.map((filename) => `'${filename}'`)
     return [
-      `yarn eslint --ext .js,.jsx,.ts,.tsx ${wrapped.join(' ')}`,
-      `yarn prettier --write ${wrapped.join(' ')}`,
+      `eslint --ext .js,.jsx,.ts,.tsx ${wrapped.join(' ')}`,
+      `prettier --write ${wrapped.join(' ')}`,
     ]
   },
 }
