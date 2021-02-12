1c4d4ad7e5471e9f8162b1032445cba50de5cf2b
babie <babie.wildheart@gmail.com>
2021-02-12 23:23:30 +0900

    improvement(lil-editor): Set container height to current height on resize event

--- TODO.md
+++ TODO.md
@@ -2,16 +2,15 @@
 
 ### Todo
 
-- [ ] [viewer] 画面が横長の時、フルブリードレイアウトにする  
 - [ ] [viewer/editor] iOS Safariでスクロールしても.containerの高さが狂わないようにする  
+- [ ] [viewer] 画面が横長の時、フルブリードレイアウトにする  
 - [ ] [viewer] Markdownのパース時、タイトルと本文以外は全部メタデータに突っ込みpagemenuで表示する  
 - [ ] mdx対応  
 - [ ] ユーザーデータはlil-commonに突っ込んだ方が良い？  
-- [ ] lil-commonをlil-shareにリネーム  
+- [ ] lil-commonをlil-dataにリネーム  
 
 ### Doing
 
-- [ ] [editor] lib/*を/に移動する  
 
 ### Done
 
@@ -20,4 +19,5 @@
 - [ ] remixiconを導入  
 - [ ] .navbarと.toolbarを.sitemenuと.pagemenuにリネーム  
 - [ ] lil-proxyを廃止してapi/index.tsをlil-editorに移す  
+- [ ] [editor] lib/*を/に移動する  
 
--- packages/lil-editor/components/layout.tsx
+++ packages/lil-editor/components/layout.tsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from 'react'
 import styles from '../styles/layout.module.css'
 import HomeSVG from 'remixicon/icons/Buildings/home-2-line.svg'
 import FileAddSVG from 'remixicon/icons/Document/file-add-line.svg'
 import FileEditSVG from 'remixicon/icons/Document/file-edit-line.svg'
 
+const getHeight = () =>
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight
+
+const useCurrentHeight = () => {
+  // save current window height in the state object
+  const [height, setHeight] = useState(getHeight())
+
+  // in this case useEffect will execute only once because
+  // it does not have any dependencies.
+  useEffect(() => {
+    // timeoutId for debounce mechanism
+    let timeoutId = null
+    const resizeListener = () => {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId)
+      // change height from the state object after 150 milliseconds
+      timeoutId = setTimeout(() => setHeight(getHeight()), 150)
+    }
+    // set resize listener
+    window.addEventListener('resize', resizeListener)
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener)
+    }
+  }, [])
+
+  return height
+}
+
 const Layout: React.FC = ({ children }) => {
+  const height = useCurrentHeight()
+
   return (
     <div className={styles.layout}>
       <nav className={styles.sitemenu}>
@@ -13,7 +49,9 @@ const Layout: React.FC = ({ children }) => {
           </li>
         </ul>
       </nav>
-      <div className={styles.container}>{children}</div>
+      <div className={styles.container} style={{ height }}>
+        {children}
+      </div>
       <div className={styles.pagemenu}>
         <ul>
           <li>
